9c8bca5fcb52fa4a60cd2014ed43a656fb69e24c
aditbiswas1 <aditbiswas1@gmail.com>
2014-01-11 22:42:25 +0530

    modified admin to show fields, search , filter registrations

--- epitch/admin.py
+++ epitch/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from epitch.models import Registration
 
-admin.site.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+	list_display = ('team_name', 'email_id', 'contact_number', 'university' , 'participant1' , 'participant2', 'participant3', 'participant4')
+	list_filter = ['team_name', 'email_id', 'contact_number', 'university' , 'participant1' , 'participant2', 'participant3', 'participant4']
+	search_fields = ['team_name', 'email_id', 'contact_number', 'university' , 'participant1' , 'participant2', 'participant3', 'participant4']
+admin.site.register(Registration, RegistrationAdmin)
